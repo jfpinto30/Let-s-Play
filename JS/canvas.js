b16b2f8d2ce065b4dc95f9e766ed68d60d2c39b8
Jfpinto <jessfpinto30@gmail.com>
2022-08-11 11:21:49 -0400

    First commit

---
+++ JS/canvas.js
@@ -0,0 +1,102 @@
+const canvas = document.getElementById("canvas");
+canvas.width =  700;
+canvas.height = 400;
+
+
+
+
+let context = canvas.getContext("2d");
+let start_background_color = "white";
+context.fillStyle = start_background_color;
+context.fillRect(0, 0, canvas.width, canvas.height);
+
+//Context
+let draw_color = "black";
+let draw_width = "2";
+let is_drawing = false;
+
+let restore_array = [];
+let index = -1;
+
+//Function to change color
+function change_color(element){
+    draw_color = element.style.background;
+}
+
+
+
+//EventListeners
+canvas.addEventListener("touchstart", start, false);
+canvas.addEventListener("touchmove", draw, false);
+canvas.addEventListener("mousedown", start, false);
+canvas.addEventListener("mousemove", draw, false);
+
+canvas.addEventListener("touchend", stop, false);
+canvas.addEventListener("mouseup", stop, false);
+canvas.addEventListener("mouseout", stop, false);
+
+
+
+//Start drawing
+function start(event) {
+    is_drawing = true;
+    context.beginPath();
+    context.moveTo(event.clientX - canvas.offsetLeft, 
+                   event.clientY - canvas.offsetTop);
+    event.preventDefault();
+}
+
+//Draw funtion
+function draw(event) {
+    if ( is_drawing ) {
+        context.lineTo(event.clientX - canvas.offsetLeft, 
+                       event.clientY - canvas.offsetTop);
+        context.strokeStyle = draw_color;
+        context.lineWidth = draw_width;
+        context.lineCap = "round";
+        context.lineJoin = "round";
+        context.stroke();
+    }
+    event.preventDefault();
+
+}
+//Stop drawing
+function stop(event) {
+    if (is_drawing) {
+        context.stroke();
+        context.closePath();
+        is_drawing = false;
+    }
+    event.preventDefault();
+
+
+    if (event.type != "mouseout") {
+      restore_array.push(context.getImageData(0, 0, canvas.width, canvas.height));
+      index +=1; 
+    }
+    
+
+}
+
+//Clear button
+function clear_canvas() {
+    context.fillStyle = start_background_color;
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    restore_array = [];
+    index = -1;
+}
+
+//Undo button
+function undo_last() {
+    if (index <= 0) {
+        clear_canvas();
+    } else {
+        index -= 1;
+        restore_array.pop();
+        context.putImageData(restore_array[index],0,0);
+    }
+}
+
+
